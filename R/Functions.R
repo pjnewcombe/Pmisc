4df7459eb3ca5643799178cd0cb58a48f598910e
Paul Newcombe <pjnewcombe@gmail.com>
2015-01-08 08:59:47 +0000

    Test

--- R/Functions.R
+++ R/Functions.R
@@ -5,7 +5,7 @@
 #' @return Bayes Factor
 #' @author Paul Newcombe
 .BayesFactor <- function(prior.prob, post.prob) {
-  priorOdds <- prior.prob/(1-prior.prob) # Test comment
+  priorOdds <- prior.prob/(1-prior.prob)
   postOdds <- post.prob/(1-post.prob)
   bayesFactor <- postOdds/priorOdds
   return(bayesFactor)
