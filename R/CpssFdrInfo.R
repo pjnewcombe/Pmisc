dd403e417228221a72a1aebe96fb49517258b142
Paul Newcombe <pjnewcombe@gmail.com>
2017-02-03 16:01:13 +0000

    Test commit from new system

--- R/CpssFdrInfo.R
+++ R/CpssFdrInfo.R
@@ -1,7 +1,7 @@
 #' @include Functions.R
 NULL
 
-#' This implements the FDR calculation developed by Shah and Samworth for Complementary Pairs Stability Selection
+#' Implements the FDR calculation developed by Shah and Samworth for Complementary Pairs Stability Selection
 #' @export
 #' @title Estimate FDR thresholds based on Complementary Pairs Stability Selection
 #' @name CpssFdrInfo
--- R/RocPlots.R
+++ R/RocPlots.R
@@ -14,6 +14,10 @@
 #' ROC curves
 #' @param lwd Line width of ROC curves (default 1)
 #' @param roc.cols Optional named vector of colours to use for the different ROC curves
+#' @param legend.name.map A character vector of different names to display for the analyses in the legend.
+#' The elements are named with the current analysis names.
+#' @param legend.cex cex argument to pass to legend - controls overall size.
+#' @param legend.text.width text.width argument to pass to legend - controls width in terms of x scale.
 #' @example Examples/RocPlots_Example.R
 #' 
 #' @author Paul Newcombe
@@ -23,11 +27,14 @@ RocPlots <- function(
   main.title=NULL,
   fpr.stop=1,
   tpr.stop=1,
-  lwd=2,
+  lwd=1,
   roc.cols=NULL,
   include.legend=TRUE,
   include.percentage.diffs=FALSE,
-  avg.type="threshold"
+  avg.type="threshold",
+  legend.name.map=NULL,
+  legend.cex=1.2,
+  legend.text.width=0.2
   ) {
   require(ROCR)
   if (is.null(names(rocr.list))) {
@@ -78,12 +85,17 @@ RocPlots <- function(
     leg.pos <- "topleft"
   }
   if (include.legend) {
+    if (!is.null(legend.name.map)) {
+      analysis.names <- legend.name.map[analysis.names]
+    }
     legend(
       leg.pos,
-      paste(formatted.aucs, analysis.names ),
+      paste(formatted.aucs, analysis.names),
       col=roc.cols,
       lwd=lwd,
-      lty=1
+      lty=1,
+      cex=legend.cex,
+      text.width=legend.text.width
     )    
   }
   
