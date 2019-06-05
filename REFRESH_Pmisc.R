b251d4f95d3d6ab0fa505e7e88cbc4416092a200
Paul Newcombe <pjnewcombe@gmail.com>
2019-06-05 17:20:15 +0100

    Retired functions RefreshPackage and WriteDescription removed.

--- REFRESH_Pmisc.R
+++ REFRESH_Pmisc.R
@@ -1,12 +1,31 @@
+library(roxygen2)
 package.name <- 'Pmisc'
-package.location <- '/Users/pauln/Dropbox/Work Projects/R Packages'
-github.username <-'pjnewcombe'
-commit.message <- NULL
-#commit.message <- "Updated ROCR plotting functions"
+package.location <- '/Users/paul/Dropbox/Work Projects/R Packages'
 
-### --- Load Pmisc and refresh the package
-library(Pmisc)
-RefreshPackage(
-  package.name=package.name,
-  package.location = "/Users/paul/Dropbox/Work Projects/R Packages/",
-  commit.message=commit.message)
+####################################
+# --- Write `DESCRIPTION' file --- #
+####################################
+write.dcf( 
+  x = list(
+    Package = package.name, 
+    Type = "Package",
+    Title = "A collection of miscellaneous functions.",
+    Version = paste("0.1",format(Sys.time(), "%d-%m-%Y"),sep="-"),
+    Date=format(Sys.time(), "%d-%m-%Y"),
+    Author = "Paul J Newcombe <paul.newcombe@mrc-bsu.cam.ac.uk>",
+    Maintainer = "Paul J Newcombe <paul.newcombe@mrc-bsu.cam.ac.uk>",
+    Description = "A collection of miscellaneous functions.",
+    License = "GPL (>=2)"),
+  file = file.path(package.location,package.name,"DESCRIPTION"),
+  append = FALSE
+)
+
+#####################################################
+# --- Run Roxygen to generate NAMESPACE and Rds --- #
+#####################################################
+roxygenise(package.dir=file.path(package.location,package.name),clean=TRUE)
+
+######################
+# --- Re-install --- #
+######################
+system(command=paste("R CMD INSTALL '",file.path(package.location,package.name),"'",sep=""))
